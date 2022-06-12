53b94d65bc11f1ac03ebfb1878814c3b57741324
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-12 15:55:47 -0400

    [FIX]

--- src/NPushMacros.h
+++ src/NPushMacros.h
@@ -1,4 +1,4 @@
 #define GET_OnReleaseEventArgs(arg) *(OnReleaseEventArgs*)arg
-#define OnRelease_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnReleaseEventArgs args_name = GET_OnReleaseEventArgs(base_##args_name); defenition }
+#define ONRELEASE_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnReleaseEventArgs args_name = GET_OnReleaseEventArgs(base_##args_name); defenition }
 #define GET_OnPushEventArgs(arg) *(OnPushEventArgs*)arg
-#define OnPush_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnPushEventArgs args_name = GET_OnPushEventArgs(base_##args_name); defenition }
+#define ONPUSH_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnPushEventArgs args_name = GET_OnPushEventArgs(base_##args_name); defenition }
