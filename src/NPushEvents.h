cbf1d2ca58a233047ffbb94afa929092da9ef564
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-14 21:32:24 -0400

    [CHANGE] Making args smaller

--- src/NPushEvents.h
+++ src/NPushEvents.h
@@ -10,13 +10,9 @@ struct OnReleaseEventArgs : public EventArgs
 {
     uint16_t holdTime;
     uint32_t pressedAt;
-    OnReleaseEventArgs(void *_sender);
-    OnReleaseEventArgs(uint16_t, uint32_t);
 };
 
 struct OnPushEventArgs : public EventArgs
 {
     uint32_t pressedAt;
-    OnPushEventArgs(void *_sender);
-    OnPushEventArgs(uint32_t);
 };
