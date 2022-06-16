7a9c689a52b8b9d42accc6caca5ba2ed4a510a45
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-06-15 20:50:34 -0400

    [CHANGE] Move to .h to allow for full user control [CHANGE] using SREG for space saving

--- src/NPush.h
+++ src/NPush.h
@@ -12,7 +12,7 @@
 #include "NPushMacros.h"
 
 #define NPUSH_MAJOR_VERSION 2
-#define NPUSH_MINOR_VERSION 0
+#define NPUSH_MINOR_VERSION 1
 #define NPUSH_PATCH_VERSION 0
 
 #ifdef NTimer_h
@@ -23,16 +23,149 @@
 	#define NPUSH_INTERVAL_CHECK() millis() - lastDebounceTime >= debounceDelay
 #endif
 
+typedef class Push;
+uint8_t PushInstanceCount = ZERO;
+Push **PushInstances = NULL;
+
+enum SREGBITS
+{
+	CURRENT,
+	PUSHED,
+	RELEASED,
+	PREVIOUS,
+	M_PUSHED,
+	M_RELEASED,
+	INVERTED
+};
+
 class Push
 {
 public:
-	Push(byte, bool, int);
-	~Push();
-	void update();
-	bool current();
-	bool pushed();
-	bool released();
-	uint16_t getHoldTime();
+	Push(byte _pin, bool _inverted, int _debounceDelay)
+		:pin(_pin), debounceDelay(_debounceDelay), onRelease(EventHandler()) ,onPush(EventHandler()), onReleaseArgs(), onPushArgs()
+	{
+		if (PushInstanceCount == ZERO)
+		{
+			PushInstances = new Push * [PushInstanceCount + 1];
+			PushInstances[PushInstanceCount] = this;
+			PushInstanceCount++;
+		}
+		else
+		{
+			Push** temp = new Push *[PushInstanceCount];
+			memmove(temp, PushInstances, sizeof(Push) * PushInstanceCount);
+
+			PushInstances = new Push *[PushInstanceCount + 1];
+			memmove(PushInstances, temp, sizeof(Push) * PushInstanceCount);
+
+			PushInstances[PushInstanceCount] = this;
+			PushInstanceCount++;
+
+			delete[] temp;
+		}
+
+		bitWrite(sreg, INVERTED, 1);
+		pinMode(_pin, (_inverted) ? INPUT_PULLUP : INPUT);
+		onReleaseArgs.sender = this;
+		onPushArgs.sender = this;
+	}
+
+	~Push()
+	{
+		for (uint8_t instance = ZERO; instance < PushInstanceCount; instance++)
+		{
+			if (PushInstances[instance] == this)
+			{
+				PushInstances[instance] = NULL;
+			}
+		}
+	}
+
+	void update()
+	{
+		if (NPUSH_INTERVAL_CHECK())
+		{
+			bitClear(sreg, M_PUSHED);
+			bitClear(sreg, M_RELEASED);
+			
+			#ifndef NTimer_h
+			lastDebounceTime = NPush_TIME();
+			#endif
+
+			bitWrite(sreg, CURRENT, ((bitRead(sreg, INVERTED)) ? !digitalRead(pin) : digitalRead(pin)));
+
+			if (!bitRead(sreg, CURRENT) && bitRead(sreg, PREVIOUS))
+			{
+				bitSet(sreg, RELEASED);
+				bitClear(sreg, PREVIOUS);
+				bitClear(sreg, PUSHED);
+				
+				onReleaseArgs.holdTime = NPush_TIME() - onReleaseArgs.pressedAt;
+				onRelease.invoke(&onReleaseArgs);
+			}
+			else
+			{
+				bitClear(sreg, RELEASED);
+			}
+
+			if (bitRead(sreg, CURRENT) && bitRead(sreg, PREVIOUS))
+			{
+				onReleaseArgs.holdTime = NPush_TIME() - onReleaseArgs.pressedAt;
+			}
+
+			if (bitRead(sreg, CURRENT) && !bitRead(sreg, PREVIOUS))
+			{
+				bitSet(sreg, PREVIOUS);
+				bitSet(sreg, PUSHED);
+
+				onReleaseArgs.pressedAt = NPush_TIME();
+				onPushArgs.pressedAt = onReleaseArgs.pressedAt;
+				onPush.invoke(&onPushArgs);
+			}
+			else
+			{
+				bitClear(sreg, PUSHED);
+			}
+		}
+	}
+
+	bool current()
+	{
+		update();
+		return bitRead(sreg, CURRENT);
+	}
+
+	bool pushed()
+	{
+		if (bitRead(sreg, M_PUSHED))
+		{
+			return false;
+		}
+		else
+		{
+			bitSet(sreg, M_PUSHED);
+			return bitRead(sreg, PUSHED);
+		}
+	}
+
+	bool released()
+	{
+		if (bitRead(sreg, M_RELEASED))
+		{
+			return false;
+		}
+		else
+		{
+			bitSet(sreg, M_RELEASED);
+			return bitRead(sreg, RELEASED);
+		}
+	}
+
+	uint16_t getHoldTime()
+	{
+		return onReleaseArgs.holdTime;
+	}
+
 	event onRelease;
 	event onPush;
 
@@ -43,13 +176,19 @@ private:
 	OnReleaseEventArgs onReleaseArgs;
 	OnPushEventArgs onPushArgs;
 	uint32_t lastDebounceTime;
-	uint16_t holdTime;
 };
 
 extern void NPush_h_userLoop();
 
-#ifndef NPush_cpp
+void loop()
+{
+	for (uint8_t instance = ZERO; instance < PushInstanceCount; instance++)
+	{
+		if (PushInstances[instance] != NULL) PushInstances[instance]->update();
+	}
+	NPush_h_userLoop();
+}
+
 #define loop NPush_h_userLoop
-#endif
 
 #endif
--- src/NPushMacros.h
+++ src/NPushMacros.h
@@ -1,4 +1,4 @@
-#define GET_OnReleaseEventArgs(arg) *(OnReleaseEventArgs*)arg
+#define GET_OnReleaseEventArgs(arg) reinterpret_c_style(OnReleaseEventArgs, arg)
 #define ONRELEASE_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnReleaseEventArgs args_name = GET_OnReleaseEventArgs(base_##args_name); defenition }
-#define GET_OnPushEventArgs(arg) *(OnPushEventArgs*)arg
+#define GET_OnPushEventArgs(arg) reinterpret_c_style(OnPushEventArgs, arg)
 #define ONPUSH_ESR(name, args_name, defenition) EVENT_FUNCTION(name, base_##args_name) { OnPushEventArgs args_name = GET_OnPushEventArgs(base_##args_name); defenition }
