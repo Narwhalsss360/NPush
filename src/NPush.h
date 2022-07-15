da18ef7152ad5da149f5ef316e27b44db260351a
Franklyn Vasquez <franklynvasquez24@gmail.com>
2022-07-15 00:15:12 -0400

    [ADD] VirtualPush

--- src/NPush.h
+++ src/NPush.h
@@ -19,6 +19,7 @@
 	#define NPUSH_INTERVAL_CHECK() millis() - lastDebounceTime >= debounceDelay
 #endif
 
+typedef bool (*VirtualPushReader) (void);
 typedef class Push;
 uint8_t PushInstanceCount = ZERO;
 Push **PushInstances = NULL;
@@ -172,6 +173,82 @@ private:
 	OnReleaseEventArgs onReleaseArgs;
 	OnPushEventArgs onPushArgs;
 	uint32_t lastDebounceTime;
+};
+
+class VirtualPush
+{
+public:
+	VirtualPush(VirtualPushReader reader, uint32_t debounceDelay)
+		: onRelease(EventHandler()), onPush(EventHandler()), reader(reader), debounceDelay(debounceDelay), lastDebounceTime(0), sreg(0), onReleaseArgs(OnReleaseEventArgs()), onPushArgs(OnPushEventArgs())
+	{
+	}
+
+	~VirtualPush()
+	{
+	}
+
+	void update()
+	{
+		if (NPUSH_INTERVAL_CHECK())
+		{
+			bitClear(sreg, M_PUSHED);
+			bitClear(sreg, M_RELEASED);
+		
+#ifndef NTimer_h
+			lastDebounceTime = time();
+#endif
+
+			bitWrite(sreg, CURRENT, reader());
+
+			if (!bitRead(sreg, CURRENT) && bitRead(sreg, PREVIOUS))
+			{
+				bitSet(sreg, RELEASED);
+				bitClear(sreg, PREVIOUS);
+				bitClear(sreg, PUSHED);
+
+				onReleaseArgs.holdTime = time() - onReleaseArgs.pressedAt;
+				onRelease.invoke(&onReleaseArgs);
+			}
+			else
+			{
+				bitClear(sreg, RELEASED);
+			}
+
+			if (bitRead(sreg, CURRENT) && bitRead(sreg, PREVIOUS))
+			{
+				onReleaseArgs.holdTime = time() - onReleaseArgs.pressedAt;
+			}
+
+			if (bitRead(sreg, CURRENT) && !bitRead(sreg, PREVIOUS))
+			{
+				bitSet(sreg, PREVIOUS);
+				bitSet(sreg, PUSHED);
+
+				onReleaseArgs.pressedAt = time();
+				onPushArgs.pressedAt = onReleaseArgs.pressedAt;
+				onPush.invoke(&onPushArgs);
+			}
+			else
+			{
+				bitClear(sreg, PUSHED);
+			}
+		}
+	}
+
+	uint16_t getHoldTime()
+	{
+		return onReleaseArgs.holdTime;
+	}
+
+	event onRelease;
+	event onPush;
+private:
+	VirtualPushReader reader;
+	uint32_t debounceDelay;
+	uint32_t lastDebounceTime;
+	byte sreg;
+	OnReleaseEventArgs onReleaseArgs;
+	OnPushEventArgs onPushArgs;
 };
 
 extern void NPush_h_userLoop();
